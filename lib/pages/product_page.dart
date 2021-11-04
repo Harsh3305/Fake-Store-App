fe68f7f757e2f1986d8544c453537099ba0355ca
Harsh3305 <harshverma3305@gmail.com>
2021-11-04 14:34:50 +0530

    Added feature of count of single product added to cart

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -4,17 +4,99 @@ import 'package:fake_store/widgets/my_theme.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({Key? key, required this.product}) : super(key: key);
   final Product product;
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  int _items_add_to_cart = 0;
+
   @override
   Widget build(BuildContext context) {
-    final int id = product.id;
-    final String title = product.title;
-    final double price = product.price;
-    final String description = product.description;
+    final int id = widget.product.id;
+    final String title = widget.product.title;
+    final double price = widget.product.price;
+    final String description = widget.product.description;
     // final String category = product.category;
-    final String image = product.image;
+    final String image = widget.product.image;
+    Widget addToCartText = const Text(
+      "Add To Cart",
+      style: TextStyle(color: Colors.white),
+    );
+    if (_items_add_to_cart > 0) {
+      addToCartText = Text(
+        "Items: $_items_add_to_cart",
+        style: const TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+      );
+    }
+
+    Widget cart_button = Container(
+      color: MyTheme.primaryLight,
+      child: TextButton(
+          onPressed: () {
+            // developer.log("Add to cart, product = $id");
+            setState(() {
+              _items_add_to_cart = 1;
+            });
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              Text(
+                "Add To Cart",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          )),
+    );
+
+    if (_items_add_to_cart > 0) {
+      cart_button = Container(
+        color: MyTheme.primaryLight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _items_add_to_cart--;
+                });
+              },
+              icon: const Icon(
+                Icons.remove,
+                color: Colors.white,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
+            addToCartText,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _items_add_to_cart++;
+                  });
+                },
+                icon: const Icon(Icons.add, color: Colors.white))
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -32,29 +114,7 @@ class ProductPage extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Text(title, style: const TextStyle(fontSize: 20)),
             ),
-            Container(
-              color: MyTheme.primaryLight,
-              child: TextButton(
-                  onPressed: () {
-                    developer.log("Add to cart, product = $id");
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.shopping_basket,
-                        color: Colors.white,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                      ),
-                      Text(
-                        "Add To Cart",
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
-                  )),
-            ),
+            cart_button,
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text((String.fromCharCodes(Runes('\u0024'))) + "$price",
