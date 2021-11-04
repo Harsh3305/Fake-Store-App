1e6785b6d9d2eb3fff067eb304a67c4dacb17546
Harsh3305 <harshverma3305@gmail.com>
2021-11-04 15:17:00 +0530

    Added padding in product display

--- lib/widgets/product_display.dart
+++ lib/widgets/product_display.dart
@@ -20,30 +20,37 @@ class _ProductDisplayState extends State<ProductDisplay> {
     final String title = widget.product.title;
     final double price = widget.product.price;
     final String image = widget.product.image;
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ProductPage(product: widget.product)));
-      },
-      child: Container(
-        // color: _backgroundColor,
-        decoration: BoxDecoration(boxShadow: const <BoxShadow>[
-          BoxShadow(
-              color: Colors.black54,
-              blurRadius: 10.0,
-              offset: Offset(0.0, 0.75))
-        ], color: _backgroundColor),
-        child: Column(
-          children: [
-            Hero(
-                tag: id,
-                child: LoadImageFromNetwork(
-                    urlOfImage: image, height: 100, width: -1)),
-            Text(title),
-            Text((String.fromCharCodes(Runes('\u0024'))) + "$price")
-          ],
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProductPage(product: widget.product)));
+        },
+        child: Container(
+          // color: _backgroundColor,
+          decoration: BoxDecoration(boxShadow: const <BoxShadow>[
+            BoxShadow(
+                color: Colors.black54,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 0.75))
+          ], color: _backgroundColor),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Hero(
+                  tag: id,
+                  child: LoadImageFromNetwork(
+                      urlOfImage: image, height: 100, width: -1)),
+              Text(title),
+              Text(
+                (String.fromCharCodes(Runes('\u0024'))) + "$price",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+              )
+            ],
+          ),
         ),
       ),
     );
