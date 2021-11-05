f63e3ce451282ef221e37f849787cccc88691d71
Harsh3305 <harshverma3305@gmail.com>
2021-11-05 09:06:24 +0530

    Added feedback page in app

--- lib/pages/feedback_page.dart
+++ lib/pages/feedback_page.dart
@@ -1,48 +1,67 @@
+import 'package:fake_store/widgets/my_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class FeedbackPage extends StatelessWidget {
+class FeedbackPage extends StatefulWidget {
   const FeedbackPage({Key? key}) : super(key: key);
+
+  @override
+  State<FeedbackPage> createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<FeedbackPage> {
+  double _rating = 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: RatingBar.builder(
-          initialRating: 3,
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            switch (index) {
-              case 0:
-                return const Icon(
-                  Icons.sentiment_very_dissatisfied,
-                  color: Colors.red,
-                );
-              case 1:
-                return const Icon(
-                  Icons.sentiment_dissatisfied,
-                  color: Colors.redAccent,
-                );
-              case 2:
-                return const Icon(
-                  Icons.sentiment_neutral,
-                  color: Colors.amber,
-                );
-              case 3:
-                return const Icon(
-                  Icons.sentiment_satisfied,
-                  color: Colors.lightGreen,
-                );
-              default:
-                return const Icon(
-                  Icons.sentiment_very_satisfied,
-                  color: Colors.green,
-                );
-            }
-          },
-          onRatingUpdate: (rating) {
-            print(rating);
-          },
+        appBar: AppBar(backgroundColor: MyTheme.primaryLight),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("Rating: $_rating",
+                  style: TextStyle(color: MyTheme.primaryLight, fontSize: 50)),
+              RatingBar.builder(
+                initialRating: 3,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  switch (index) {
+                    case 0:
+                      return const Icon(
+                        Icons.sentiment_very_dissatisfied,
+                        color: Colors.red,
+                      );
+                    case 1:
+                      return const Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: Colors.redAccent,
+                      );
+                    case 2:
+                      return const Icon(
+                        Icons.sentiment_neutral,
+                        color: Colors.amber,
+                      );
+                    case 3:
+                      return const Icon(
+                        Icons.sentiment_satisfied,
+                        color: Colors.lightGreen,
+                      );
+                    default:
+                      return const Icon(
+                        Icons.sentiment_very_satisfied,
+                        color: Colors.green,
+                      );
+                  }
+                },
+                onRatingUpdate: (rating) {
+                  setState(() {
+                    _rating = rating;
+                  });
+                },
+              ),
+            ],
+          ),
         ));
   }
 }
